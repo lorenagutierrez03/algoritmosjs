8c51521bb24d5dd9b721c0bb6cf44b723ac271d4
lorenagutierrez03 <102312706+lorenagutierrez03@users.noreply.github.com>
2022-03-24 11:06:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+/*
+ // SUMA
+function suma () {
+var a =0;
+var b =0;
+var suma = 0;
+var resta=0;
+var multiplicacion=0;
+var division =0;
+
+// parseInt es para covertir string en enteros 
+a = parseInt(prompt("ingrese el primer valor"));
+b = parseInt(prompt("por favor ingrese el segundo valor"));
+
+suma =a + b;
+resta = a-b;
+multiplicacion = a*b;
+division = a /b;
+
+alert ("el resultado de la suma es:"+ suma);
+alert ("el resultado de la resta es:"+ resta);
+alert ("el resultado de la multiplicacion es:"+ multiplicacion);
+alert ("el resultado de la division es:"+ division);
+
+}
+ num1 = parseInt(prompt("Ingrese el primer número: "));
+ num2 = parseInt(prompt("Ingrese el segundo número: "));
+ if(num1 > num2){
+     alert ("el número mayor es:"+ num1);
+ }else {
+    alert ("el número mayor es:"+ num2);
+ }
+
+ //Determinar cual es el numero menor de tres numeros ingresados por el usuario//
+num1 = parseInt(prompt("Ingrese el primer número: "));
+ num2 = parseInt(prompt("Ingrese el segundo número: "));
+ num3 = parseInt(prompt("Ingrese el tercer número: "));
+
+ if(num1 <= num2 && num1 <= num3){
+     menor = num1;
+ }else if(num2 <= num1 && num2 <= num3){
+     menor = num2;
+ }else{
+     menor = num3;
+ }
+ alert ("el número menor es:"+ menor);
+
+ //Determinar si un numero es par o impar//
+     let numero;
+      numero=parseInt(prompt("introduzca un numero"));
+      if (numero % 2==0){
+          alert ("El numero " +numero+ " es par");
+      } else{
+          alert ("El numero " +numero+ " es impar");
+      }
+
+ //Determinar el area de un triangulo
+ let base = parseInt(prompt("introduzca la base del triangulo"));
+ let altura = parseInt(prompt("introduzca la altura del triangulo"));
+ let area;
+ area = (base * altura) /2;
+ alert ("El area del triangulo es: " + area + "cm²");
+ //alt + 253*/
